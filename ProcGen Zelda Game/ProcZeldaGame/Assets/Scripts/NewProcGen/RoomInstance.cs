baa17444cebd7f4430e1e4cc5c4d044ae6da0b7e
rosie_mable <sciarrillo.nella.jobs@gmail.com>
2018-11-17 01:05:01 +0000

    THE PROCGEN WORKS PROPERLY NOW :D

--- ProcGen Zelda Game/ProcZeldaGame/Assets/Scripts/NewProcGen/RoomInstance.cs
+++ ProcGen Zelda Game/ProcZeldaGame/Assets/Scripts/NewProcGen/RoomInstance.cs
@@ -14,12 +14,10 @@ public class RoomInstance : MonoBehaviour {
 
     public bool doorTop, doorBot, doorLeft, doorRight;
 
+    public GameObject doorU, doorD, doorL, doorR;
 
-    [SerializeField]
-    private GameObject doorU, doorD, doorL, doorR;
 
-    [SerializeField]
-    private GameObject doorWall;
+    public GameObject doorWall;
 
 
     [SerializeField]
@@ -27,7 +25,7 @@ public class RoomInstance : MonoBehaviour {
 
     private float tileSize = 16;
 
-    public Vector2 roomSizeInTiles = new Vector2(9, 17);
+ Vector2 roomSizeInTiles = new Vector2(9, 17);
 
     public void Setup(Texture2D _tex, Vector2 _gridPos, int _type, bool _doorTop, bool _doorBot, bool _doorLeft, bool _doorRight)
     {
@@ -45,22 +43,24 @@ public class RoomInstance : MonoBehaviour {
 
     private void MakeDoors()
     {
+ 
         //top door, get position then spawn
-        Vector3 spawnPos = transform.position + Vector3.up * (roomSizeInTiles.y / 4 * tileSize) - Vector3.up * (tileSize / 4);
+        Vector3 spawnPos = new Vector3(transform.position.x, transform.position.y, -5) + Vector3.up * (roomSizeInTiles.y / 4 * tileSize) - Vector3.up * (tileSize / 4);
         PlaceDoor(spawnPos, doorTop, doorU);
         //bottom door
-        spawnPos = transform.position + Vector3.down * (roomSizeInTiles.y / 4 * tileSize) - Vector3.down * (tileSize / 4);
+        spawnPos = new Vector3(transform.position.x, transform.position.y, -5) + Vector3.down * (roomSizeInTiles.y / 4 * tileSize) - Vector3.down * (tileSize / 4);
         PlaceDoor(spawnPos, doorBot, doorD);
         //right door
-        spawnPos = transform.position + Vector3.right * (roomSizeInTiles.x * tileSize) - Vector3.right * (tileSize);
+        spawnPos = new Vector3(transform.position.x, transform.position.y, -5) + Vector3.right * (roomSizeInTiles.x * tileSize) - Vector3.right * (tileSize);
         PlaceDoor(spawnPos, doorRight, doorR);
         //left door
-        spawnPos = transform.position + Vector3.left * (roomSizeInTiles.x * tileSize) - Vector3.left * (tileSize);
+        spawnPos = new Vector3(transform.position.x, transform.position.y, -5) + Vector3.left * (roomSizeInTiles.x * tileSize) - Vector3.left * (tileSize);
         PlaceDoor(spawnPos, doorLeft, doorL);
     }
 
     void PlaceDoor(Vector3 spawnPos, bool door, GameObject doorSpawn)
     {
+
         if (door)
         {
             Instantiate(doorSpawn, spawnPos, Quaternion.identity).transform.parent = transform;
@@ -113,9 +113,9 @@ public class RoomInstance : MonoBehaviour {
 
         Vector3 ret;
         //find difference between the corner of the texture and the center of this object
-        Vector3 offset = new Vector3((-roomSizeInTiles.x + 1) * tileSize, (roomSizeInTiles.y / 4) * tileSize - (tileSize / 4), 0);
+        Vector3 offset = new Vector3((-roomSizeInTiles.x + 1) * tileSize, (roomSizeInTiles.y / 4) * tileSize - (tileSize / 4), -5);
         //find scaled up position at the offset
-        ret = new Vector3(tileSize * (float)x, -tileSize * (float)y, 0) + offset + transform.position;
+        ret = new Vector3(tileSize * (float)x, -tileSize * (float)y, -5) + offset + transform.position;
         return ret;
     }
 
