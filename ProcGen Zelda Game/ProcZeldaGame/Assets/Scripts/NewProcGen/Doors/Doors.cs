22846f3a7c8ec6ef59258c1c8f5d36a2d440d9f9
rosie_mable <sciarrillo.nella.jobs@gmail.com>
2018-11-21 23:17:39 +0000

    THE CAMERA WORKS
    IT WORKS!
    FINALLY QWQ

--- ProcGen Zelda Game/ProcZeldaGame/Assets/Scripts/NewProcGen/Doors/Doors.cs
+++ ProcGen Zelda Game/ProcZeldaGame/Assets/Scripts/NewProcGen/Doors/Doors.cs
@@ -7,17 +7,33 @@ public class Doors : MonoBehaviour
 
     protected Camera camera;
 
+    protected GameObject Hero;
+
+    protected Vector3 moveJumpDoor = Vector2.zero;
     protected Vector3 moveJump = Vector2.zero;
     protected float horMove, vertMove;
 
     [SerializeField]
+    protected float Time = 20f;
+
+    [SerializeField]
+    protected Vector2 distanceBetweenDoors;
+
+    [SerializeField]
     protected Vector2 distanceBetweenRooms;
+
+    protected float ValueX;
+
+    protected float ValueY;
 
     void Start()
     {
-        SheetAssigner SA = FindObjectOfType<SheetAssigner>();
-        distanceBetweenRooms = new Vector2(418f, 212f);
-        moveJump = new Vector3(distanceBetweenRooms.x, distanceBetweenRooms.y, 0); //distance b/w rooms: to be used for movement
+        distanceBetweenRooms = new Vector2(209f, 106);
+        distanceBetweenDoors = new Vector2( 100 , 55);
+
+
+        moveJump = new Vector3(distanceBetweenRooms.x, distanceBetweenRooms.y, -20); //distance b/w rooms: to be used for movement
+        moveJumpDoor = new Vector3(distanceBetweenDoors.x, distanceBetweenDoors.y, -20);
     }
 
     private void Update()
@@ -25,41 +41,60 @@ public class Doors : MonoBehaviour
 
         camera = Camera.main;
 
+        Hero = FindObjectOfType<Hero>().gameObject;
+
     }
 
     protected void MoveUp()
     {
         Vector3 tempPos = camera.transform.position;
-        Vector3 UpMove = Vector3.up * moveJump.y;
-        tempPos = UpMove;
+        ValueY = tempPos.y += distanceBetweenRooms.y;
+        Mathf.Lerp(camera.transform.position.y, ValueY, Time);
         tempPos.z = -20f;
         camera.transform.position = tempPos;
+
+
+        Vector3 tempPosPC = Hero.transform.position;
+        tempPosPC.y += distanceBetweenDoors.y;
+        Hero.transform.position = tempPosPC;
     }
 
     protected void MoveDown()
     {
         Vector3 tempPos = camera.transform.position;
-        Vector3 DownMove = Vector3.down * moveJump.y;
-        tempPos += DownMove;
+        ValueY = tempPos.y -= distanceBetweenRooms.y;
+        Mathf.Lerp(camera.transform.position.y,ValueY, Time);
         tempPos.z = -20f;
         camera.transform.position = tempPos;
+
+        Vector3 tempPosPC = Hero.transform.position;
+        tempPosPC.y -= distanceBetweenDoors.y;
+        Hero.transform.position = tempPosPC;
     }
 
     protected void MoveLeft()
     {
         Vector3 tempPos = camera.transform.position;
-        Vector3 LeftMove = Vector3.right * -moveJump.x;
-        tempPos += LeftMove;
+        ValueX = tempPos.x -= distanceBetweenRooms.x;
+        Mathf.Lerp(camera.transform.position.x, ValueX, Time);
         tempPos.z = -20f;
         camera.transform.position = tempPos;
+
+        Vector3 tempPosPC = Hero.transform.position;
+        tempPosPC.x -= distanceBetweenDoors.x;
+        Hero.transform.position = tempPosPC;
     }
 
     protected void MoveRight()
     {
         Vector3 tempPos = camera.transform.position;
-        Vector3 RightMove = Vector3.right * moveJump.x;
-        tempPos += RightMove;
+        ValueX = tempPos.x += distanceBetweenRooms.x;
+        Mathf.Lerp(camera.transform.position.x, ValueX, Time);
         tempPos.z = -20f;
         camera.transform.position = tempPos;
+
+        Vector3 tempPosPC = Hero.transform.position;
+        tempPosPC.x += distanceBetweenDoors.x;
+        Hero.transform.position = tempPosPC;
     }
 }
